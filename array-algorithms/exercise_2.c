19e3ff30c6d879dda851aea8d6adf578ff3a5c91
carminedev <carminech@proton.me>
2026-02-21 11:10:26 +0100

    Add solution for exercise_2: Generate Fibonacci sequence in an array

--- array-algorithms/exercise_2.c
+++ array-algorithms/exercise_2.c
@@ -6,6 +6,13 @@
  * 2. Stores the first `n` Fibonacci numbers in an array
  * 3. Prints the sequence
  *
+ * The Fibonacci Sequence is defined:
+ *   - F(0) = 1
+ *   - F(1) = 1
+ *   - F(n) = F(n - 1) + F(n - 2)
+ *
+ * Each amount of the sequence is obtained by summing the two previous one
+ *
  * Requirements:
  *  - Initialize the first two elements explicitly
  *  - Compute each new element using the two previous ones
@@ -13,23 +20,40 @@
 
 int main() {
 
+    // Number of elements of the sequence to generate
     int number_of_fibonacci_numbers = 0;
+
+    // Read Input
     printf("Insert the number of fibonacci numbers to print: ");
     scanf("%d", &number_of_fibonacci_numbers);
 
+    // Array to store the sequence
+    // The array size is equal to the number of elements requested
     int fibonacci_sequence[number_of_fibonacci_numbers];
 
-    // Initial values
+    // Initial explicit two values
     fibonacci_sequence[0] = 1;
     fibonacci_sequence[1] = 1;
 
+    // Compute the remaining numbers of the sequence
+    // Starting from index 2, each element is calculated
+    // as the sum of the two previous 
+    //
+    //     fibonacci_sequence[i] = fibonacci_sequence[i - 1]
+    //                          + fibonacci_sequence[i - 2]
+    // The loop fills the array sequentially
     for (int i = 2; i < number_of_fibonacci_numbers; i++) {
         fibonacci_sequence[i] = fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2];
     }
 
+    printf("[");
     for (int i = 0; i < number_of_fibonacci_numbers; i++) {
-        printf("%d, ", fibonacci_sequence[i]);
+        printf("%d", fibonacci_sequence[i]);
+        if (i < number_of_fibonacci_numbers - 1) {
+            printf(", ");
+        }
     }
+    printf("]\n");
 
     return 0;
 }
